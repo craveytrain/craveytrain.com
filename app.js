961616c2abde7e6044e6e3a916befb46f34dc798
Mike Cravey <techie@craveytrain.com>
2011-03-17 01:17:09 -0500

    Getting basic views working

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-var app = require('express').createServer(),
+var express = require('express'),
 		fs = require('fs'),
 		md = require('markdown').markdown;
+		
+var app = express.createServer(express.static(__dirname + '/public'));
 
-// register .md so that markdown may comply
-// with the express view system by implementing
-// a .compile() method
 app.register('.md', {
-  compile: function(str, options){
-    var html = md.toHTML(str);
-    return function(locals){
-      return html.replace(/\{([^}]+)\}/g, function(_, name){
-        return locals[name];
-      });
-    };
-  }
+	compile: function(str, options){
+		var html = md.toHTML(str);
+		return function(locals){
+			return html.replace(/\{([^}]+)\}/g, function(_, name){
+				var cur = locals;
+				name.split('.').forEach(function(prop) {
+					cur = cur[prop]
+				})
+				return cur;
+			});
+		};
+	}
 });
+
+Object.defineProperty(Object.prototype, "extend", {
+	enumerable: false,
+	value: function(from) {
+		var props = Object.getOwnPropertyNames(from);
+		var dest = this;
+		props.forEach(function(name) {
+			if (name in dest) {
+				var destination = Object.getOwnPropertyDescriptor(from, name);
+				Object.defineProperty(dest, name, destination);
+			}
+		});
+		return this;
+	}
+});
+
 
 app.set('view engine', 'jade');
 
@@ -35,24 +54,27 @@ var posts = {
 };
 
 app.get('/', function(req, res) {
-	res.send('hello world');
+	res.render('index');
 });
 
 app.get('/about', function(req, res) {
-	res.render('about');
+	var page = { title: 'About' };
+	res.render('about.md', { layout: 'layout.jade', page: page });
 });
 
 app.get('/posts', function(req, res) {
-	res.partial('posts', posts);
+	var page = { title: 'Posts' };
+	res.render('posts', {posts: posts, page: page});
 });
 
 app.get('/posts/:slug', function(req, res) {
 	// Add if for both slug in posts and md file exists
-	var post = posts[req.params.slug];	
+	var post = posts[req.params.slug];
 	fs.readFile(__dirname + '/posts/' + req.params.slug + '.md', 'UTF-8', function(e, d) {
 		if (e) console.log(e);
 		post.content = md.toHTML(d);
-		res.partial('posts/post', post);
+		var page = {};
+		res.render('posts/post', { post: post, page: page });
 	});
 });
 
