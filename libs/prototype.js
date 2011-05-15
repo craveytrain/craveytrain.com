6eba48e9b5bece2fcb4ef2ba9da9bb0286db45d3
Mike Cravey <techie@craveytrain.com>
2011-05-14 23:30:24 -0500

    Adding plurailze method for strings

--- libs/prototype.js
+++ libs/prototype.js
@@ -33,3 +33,31 @@ Object.defineProperty(String.prototype, 'unurlify', {
 		return this.replace(/\-/g, ' ');
 	}
 });
+
+Object.defineProperty(String.prototype, 'pluralize', {
+	enumerable: false,
+	value: function(/* Int */count, /* String? */plural) {
+		var text = this, lastIdx;
+		
+		if (count !== 1) {
+			if (plural) return plural; // String
+			
+			lastIdx = text.length - 1;
+			switch (text[lastIdx]) {
+				case 'x':
+				case 'h':
+				case 's':
+					text += 'es';
+					break;
+				case 'y':
+					text = text.substring(0, lastIdx) + 'ies';
+					break;
+				default:
+					text += 's';
+					break;
+			}
+		}
+		
+		return text.toString(); //String
+	}
+});
