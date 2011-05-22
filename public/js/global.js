acf893c5ab068042388175dc83cea2e6b995a570
Mike Cravey <techie@craveytrain.com>
2011-05-22 17:31:54 -0500

    Adding humble beginnings of real time markdown processor

---
+++ public/js/global.js
@@ -0,0 +1,7 @@
+/* TODO:
+Setup event listeners on the comment form for focus, blur & keypress.
+On focus, show previewer.
+On blur, hide previewer (and clean it out).
+On keypress, grab the value, append the event data value and send it to showdown.
+Once that text is processed, drop it in the previewer.
+*/
