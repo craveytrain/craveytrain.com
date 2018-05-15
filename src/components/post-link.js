0a9e14ede01bc072dbbb68467d57208b9b2de07e
Mike Cravey <techie@craveytrain.com>
2018-05-15 00:31:17 -0500

    Removing `<time>` from post-link

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -8,7 +8,7 @@ const PostLink = ({ post }) => (
         {post.frontmatter.title}
       </Link>
     </h2>
-    <p className="timestamp"><time dateTime={post.frontmatter.date} pubdate>{post.frontmatter.date}</time></p>
+    <p className="timestamp">{post.frontmatter.date}</p>
     {post.frontmatter.tags && (
       <footer>
         <p className="meta">Posted in {post.frontmatter.tags.map((tag, i) =>
