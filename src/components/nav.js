3b07d2995b3be82592aa1f13944f6fc67f5e8d66
Mike Cravey <mcravey@expedia.com>
2018-05-30 00:29:01 -0500

    Adds home page hero

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,8 +3,9 @@ import styles from './nav.module.css';
 import Link from 'gatsby-link';
 import Logo from '../../img/logo.svg';
 import SearchIcon from '../../img/search.svg';
+import LogoLong from '../../img/logo-long.svg';
 
-const Nav = ({title}) => {
+const Nav = ({title, location}) => {
   const navItems = [
     {
       name: 'Posts',
@@ -19,27 +20,39 @@ const Nav = ({title}) => {
       url: '/#contact'
     }
   ];
+
   return (
-    <nav className={styles.nav}>
-      <ul className={styles.navList}>
-        <li className={styles.navListItem}>
-          <Link to="/" className={styles.navLink}>
-            <Logo className={styles.logo} />
-            <span className={styles.siteTitle}>{title}</span>
-          </Link>
-        </li>
-        {navItems.map(navItem => <li key={navItem.url} className={styles.navListItem}>
-          <Link to={navItem.url} className={styles.navLink}>
-            {navItem.name}
-          </Link>
-        </li>)}
-        <li className={styles.navListItem}>
-          <a className={styles.navLink} href="https://www.google.com/search?sitesearch=craveytrain.com">
-            <SearchIcon className={styles.searchIcon} />
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      {location.pathname === '/' &&
+        <div className={styles.hero}>
+          <h1 className={styles.heroLogo}>
+            <LogoLong />
+          </h1>
+        </div>
+      }
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          {location.pathname !== '/' &&
+            <li className={styles.navListItem}>
+              <Link to="/" className={styles.navLink}>
+                <Logo className={styles.logo} />
+                <span className={styles.siteTitle}>{title}</span>
+              </Link>
+            </li>
+          }
+          {navItems.map(navItem => <li key={navItem.url} className={styles.navListItem}>
+            <Link to={navItem.url} className={styles.navLink}>
+              {navItem.name}
+            </Link>
+          </li>)}
+          <li className={styles.navListItem}>
+            <a className={styles.navLink} href="https://www.google.com/search?sitesearch=craveytrain.com">
+              <SearchIcon className={styles.searchIcon} />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
