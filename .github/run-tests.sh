e91514e0e2ac5190565e2e543bc0208eeebe29cb
Mike Cravey <mcravey@expedia.com>
2019-06-17 16:30:08 -0500

    Adds passing only github action

---
+++ .github/run-tests.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# yarn install --force --frozen-lockfile
+# # test runs build and lint first
+# npm run test
+
+exit 0
