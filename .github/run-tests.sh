19ae28249335e64beeb28bf7004e70ae4bd10af7
Mike Cravey <mike@craveytrain.com>
2020-07-15 11:56:46 -0500

    Adds netlify config rather than website gui (#22)

--- .github/run-tests.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# Exit immediately if a command exits with a non-zero status.
-set -e
-
-# yarn install --force --frozen-lockfile
-# # test runs build and lint first
-# npm run test
-
-exit 0
