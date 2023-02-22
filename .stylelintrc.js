5cac82dbfecba98daa9f56df042969ce283f9ed0
Mike Cravey <mike@craveytrain.com>
2023-02-22 11:34:59 -0600

    Adds PR test action
    - upgrades node version to 19.6.1

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-	extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+	extends: ['stylelint-config-standard'],
 	rules: {
 		'font-family-no-missing-generic-family-keyword': null,
 	},
