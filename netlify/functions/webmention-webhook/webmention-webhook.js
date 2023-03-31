01c0c4453197798eafad942808700b2fe1c6b28e
Mike Cravey <mike@craveytrain.com>
2023-03-31 17:47:09 -0500

    Adds webmention netlify function

---
+++ netlify/functions/webmention-webhook/webmention-webhook.js
@@ -0,0 +1,38 @@
+const { env } = require('process')
+const fetch = require('node-fetch')
+
+const handler = async function (event) {
+	let secret
+	try {
+		const { secret: _secret } = JSON.parse(event.body)
+		secret = _secret
+	} catch (error) {
+		return { statusCode: 400 }
+	}
+
+	if (secret !== env.WEBMENTION_IO_TOKEN) {
+		return { statusCode: 401 }
+	}
+
+	try {
+		const response = await fetch(
+			'https://api.github.com/repos/craveytrain/craveytrain.com/dispatches',
+			{
+				method: 'POST',
+				headers: {
+					Authorization: `token ${env.GITHUB_WEBMENTION_TOKEN}`,
+				},
+				body: '{ "event_type": "add-webmention" }',
+			}
+		).then(response => response.text())
+
+		if (response) {
+			return { statusCode: 500, body: response }
+		}
+		return { statusCode: 200, body: 'ok' }
+	} catch (error) {
+		return { statusCode: 500, body: error.message }
+	}
+}
+
+module.exports = { handler }
